45bea19c9926c9a479cf7e96057d219c2fabd1b7
Anh Thu <htathu56@gmail.com>
2026-02-26 20:24:46 +0700

    BT2

---
+++ index2.js
@@ -0,0 +1,4 @@
+function introduce(name, age, address) {
+    console.log(`Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi, tôi đang sinh sống ở ${address}`);
+}
+introduce("Anh Thư", "24", "Quận 12");
